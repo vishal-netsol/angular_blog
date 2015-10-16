5d53f5f766e8063bf7ee52baee83e35a315c4b64
vishal <vishal.sharma@netsolutions.in>
2015-10-16 13:22:52 +0530

    integrated cap for production env.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,11 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+current_branch = `git branch`.match(/\* (\S+)\s/m)[1]
+set :branch, ENV['branch'] || current_branch || 'master'
+role :app, %w(roruser@192.168.0.202)
+role :web, %w(roruser@192.168.0.202)
+role :db,  %w(roruser@192.168.0.202)
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +16,9 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# server 'frdatalayer.netsolutions.in', user: 'roruser', roles: %w(web app)
+server '192.168.0.202', user: ['roruser', 'jenkins'], roles: %w(web app)
+set :enable_ssl, false
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
